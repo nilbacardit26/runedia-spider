ad010330312503a988cf3db00f26a2be05fdb7ff
Nil Bacardit <n.bacardit@iskra.cat>
2020-06-21 19:12:41 +0200

    parsing races spider

--- runedia_spider/runedia_spider/settings.py
+++ runedia_spider/runedia_spider/settings.py
@@ -13,6 +13,7 @@ BOT_NAME = 'runedia_spider'
 
 SPIDER_MODULES = ['runedia_spider.spiders']
 NEWSPIDER_MODULE = 'runedia_spider.spiders'
+FEED_EXPORT_ENCODING = 'utf-8'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
--- runedia_spider/runedia_spider/spiders/races_spider.py
+++ runedia_spider/runedia_spider/spiders/races_spider.py
@@ -2,14 +2,14 @@ import scrapy
 import arrow
 
 
-class QuotesSpider(scrapy.Spider):
+class RunediaSpider(scrapy.Spider):
     name = "races_spider"
 
-    def get_urls(first_year=1999, last_year=None):
+    def get_urls(self, first_year=2019, last_year=None):
         actual_year = last_year
         if not last_year:
             actual_year = arrow.utcnow().year
-        base_url = "https://runedia.mundodeportivo.com/en/races-calendar/pais/comunidad/provincia/tipo/distancia/{year}-{month}/0/0/"
+        base_url = "https://runedia.mundodeportivo.com/en/races-calendar/spain/comunidad/provincia/mountain-races/distancia/{year}-{month}/0/0/"
         urls_list = []
         for year in range(first_year, actual_year + 1):
             for month in range(1, 13):
@@ -21,8 +21,36 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        curses = response.css("div.item-cursa")
+        year = response.request.url.split("/")[10].split("-")[0]
+        for cursa in curses:
+            print(cursa.css("span::text").getall())
+            # Parse the distance
+            distance = "Unknown"
+            all_spans = cursa.css("span::text").getall()
+            for span in all_spans:
+                # Distances end with m from meters
+                if span.endswith("m"):
+                    distance_int = span[:len(span) - 1].strip()
+                    if distance_int.isdigit():
+                        distance = span
+                        break
+                else:
+                    distance_int = span.strip()
+                    if distance_int.isdigit():
+                        distance = span
+                        break
+            location = cursa.css("span.lloc::text").get()
+            delimiter = None
+            if ',' in location:
+                delimiter = ','
+            yield {
+                "day": cursa.css("span.dia::text").get(),
+                "month": cursa.css("span.mes::text").get(),
+                "province": location.split(delimiter)[0],
+                "town": location.split(delimiter)[1],
+                "title": cursa.css("a::attr(title)").get(),
+                "distance": distance,
+                "status": cursa.css("img.icono::attr(title)").get(),
+                "year": year
+            }
